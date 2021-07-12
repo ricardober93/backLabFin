cfd4ecf52f379c5855a06c9656700ce975606cc0
ricardober93 <ricardobzorro@gmail.com>
2021-07-12 01:19:33 +0000

    faet *(

--- database/migrations/1625742564408_productos.ts
+++ database/migrations/1625742564408_productos.ts
@@ -6,7 +6,9 @@ export default class Productos extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.string('name', 255).notNullable()
+      table.integer('quantity').notNullable()
+      table.integer('price').notNullable()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
