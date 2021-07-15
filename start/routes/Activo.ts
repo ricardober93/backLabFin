b5bd7ab6e39aded8f1f43621e47288b9015762f1
ricardober93 <ricardobzorro@gmail.com>
2021-07-15 01:46:12 +0000

    feat: se enlaza la el controlador con la ruta

--- start/routes/Activo.ts
+++ start/routes/Activo.ts
@@ -2,4 +2,5 @@ import Route from "@ioc:Adonis/Core/Route"
 import ProyeccionActivosController from "App/Controllers/Http/ProyeccionActivosController";
 
 const activoController = new ProyeccionActivosController()
+Route.get("/proyeccion/activo", activoController.index);
 Route.post("/proyeccion/activo", activoController.create);
