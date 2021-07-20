207f2cb1f40198e918852670101d4658c5acc93e
ricardober93 <ricardobzorro@gmail.com>
2021-07-20 15:24:01 +0000

    fix: se mantienen los tipados de los activos, pasivos y patrimonios

--- app/Controllers/Http/BaseInicialsController.ts
+++ app/Controllers/Http/BaseInicialsController.ts
@@ -2,12 +2,25 @@ import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Activo from "App/Models/Activo";
 import Pasivo from "App/Models/Pasivo";
 import Patrimonio from "App/Models/Patrimonio";
+
+export interface Iactivos {
+  name: string;
+  valor: number;
+}
+
+export interface Ipasivos {
+  name: string;
+  valor: number;
+}
+export interface Ipatrimonio {
+  name: string;
+  valor: number;
+}
 export default class BaseInicialsController {
   public async create({ request, response }: HttpContextContract) {
-
-    const activos = request.input("activos");
-    const pasivos= request.input("pasivos");
-    const patrimonio = request.input("patrimonio");
+    const activos: Iactivos[] = request.input("activos");
+    const pasivos: Ipasivos[] = request.input("pasivos");
+    const patrimonio: Ipatrimonio[] = request.input("patrimonio");
 
     // Varibales para calcular la regla de negocios
     //recorrer cada arreglo para que obtener el total de activos, pasivos, patrimonio.
@@ -24,19 +37,24 @@ export default class BaseInicialsController {
       0
     );
 
-    let sum = totalPatrimonio+totalPasivos
+    let sum = totalPatrimonio + totalPasivos;
 
-    if(totalActivos !== sum ){
-      response.status(400).json({message:"La suma de los pasivos y patrimonio no son iguales a los activos"})
-      return
+    if (totalActivos !== sum) {
+      response
+        .status(400)
+        .json({
+          message:
+            "La suma de los pasivos y patrimonio no son iguales a los activos",
+        });
+      return;
     }
 
-    await Activo.createMany(activos)
+    await Activo.createMany(activos);
 
-    await Pasivo.createMany(pasivos)
+    await Pasivo.createMany(pasivos);
 
-    await Patrimonio.createMany(patrimonio)
+    await Patrimonio.createMany(activos);
 
-    response.status(200).json({message:"Base inicial creaado"})
+    response.status(200).json({ message: "Base inicial creaado" });
   }
 }
