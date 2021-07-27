a5fba52ae774025e00ec5b4b480a9374690db57e
ricardober93 <ricardobzorro@gmail.com>
2021-07-27 03:02:52 +0000

    feat: se actualizan las tablas con el campo de la relacion

--- database/migrations/1625742585181_varaibles.ts
+++ database/migrations/1625742585181_varaibles.ts
@@ -7,6 +7,12 @@ export default class Varaibles extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
+
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('users.id')
+        .onDelete('CASCADE') // delete profile when user is deleted
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
