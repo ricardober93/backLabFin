508a3f601620d658cedbe6796b7a0457d52fa932
ricardober93 <ricardobzorro@gmail.com>
2021-07-27 03:06:43 +0000

    feat: se relacion la tabla de otras variables con la tabla usuarios

--- app/Models/OtherVaraible.ts
+++ app/Models/OtherVaraible.ts
@@ -1,9 +1,16 @@
+import { User } from 'App/Models/user';
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class OtherVaraible extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
