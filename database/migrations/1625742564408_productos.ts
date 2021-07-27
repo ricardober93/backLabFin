d2f69f292b6be50c1f5a8a568b627548f1e2f2d7
ricardober93 <ricardobzorro@gmail.com>
2021-07-27 03:00:44 +0000

    feat: se crea la tabla con la relacion de los productos y los usuarios

--- database/migrations/1625742564408_productos.ts
+++ database/migrations/1625742564408_productos.ts
@@ -13,6 +13,11 @@ export default class Productos extends BaseSchema {
       table.integer('rate_raise')
       table.integer('rate_of_sale')
       table.integer('rate_of_purchases')
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('users.id')
+        .onDelete('CASCADE') // delete profile when user is deleted
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
