53a170da95fe473fd2db2a0fc3fa867ac89ad87b
ricardober93 <ricardobzorro@gmail.com>
2021-07-27 03:02:24 +0000

    feat: se han creado las relaciones con todas las tablas

--- app/Models/user.ts
+++ app/Models/user.ts
@@ -4,8 +4,15 @@ import {
   column,
   beforeSave,
   BaseModel,
+  hasMany,
+  HasMany,
 } from '@ioc:Adonis/Lucid/Orm'
 
+import Activo from 'App/Models/Activo';
+import Pasivo from './Pasivo';
+import Patrimonio from './Patrimonio';
+import Producto from './Producto';
+import Salario from './Salario';
 export default class user extends BaseModel {
   @column({ isPrimary: true })
   public id: number
@@ -19,11 +26,29 @@ export default class user extends BaseModel {
   @column()
   public rememberMeToken?: string
 
+  @column()
+  public isActive: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Activo)
+  public activos: HasMany<typeof Activo>
+
+  @hasMany(() => Pasivo)
+  public pasivos: HasMany<typeof Pasivo>
+
+  @hasMany(() => Patrimonio)
+  public patrimonios: HasMany<typeof Patrimonio>
+
+  @hasMany(() => Producto)
+  public productos: HasMany<typeof Producto>
+
+  @hasMany(() => Salario)
+  public salarios: HasMany<typeof Salario>
 
   @beforeSave()
   public static async hashPassword (user: user) {
