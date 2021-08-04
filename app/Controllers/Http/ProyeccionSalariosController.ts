d82c5908b01b5f76e0b0975ce51a2cf2ab6152f5
ricardober93 <ricardobzorro@gmail.com>
2021-08-04 11:50:27 +0000

    fix: se refactorizan los metodos para que solo los puedan realizar desde el usuario

--- app/Controllers/Http/ProyeccionSalariosController.ts
+++ app/Controllers/Http/ProyeccionSalariosController.ts
@@ -1,41 +1,64 @@
+import  User from 'App/Models/user';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Salario from 'App/Models/Salario';
 
 
 const salarioModel =  new Salario();
 export default class ProyeccionSalariosController {
-  public async index ({ response }: HttpContextContract) {
-    const salarios = await Salario.all();
-    if (salarios.length < 0) {
+  public async index ({ response , auth}: HttpContextContract) {
+    const user = await User.find(auth.use("api")?.user?.id);
+
+    await user?.load("salarios");
+    if (user?.salarios === null) {
       response.status(400).json({ message: "no hay salarios para mostrar" });
     }
-    if (salarios.length > 0) {
-      response.status(200).json(salarios);
+    if (user?.salarios !== null) {
+      response.status(200).json(user?.salarios);
     }
   }
 
-  public async create ({request, response}: HttpContextContract) {
+  public async create ({request, response, auth}: HttpContextContract) {
+    const user: User = auth.use("api")?.user;
 
-    const name = request.input("name");
-    const salary = request.input("salary")
-    const dayWorks = request.input("dayWorks")
-    const pension = request.input("pension")
-    const salud = request.input("salud")
-    const transport = request.input("transport")
-    const comision = request.input("comision")
+    const name = request?.input("name");
+    const salary = request?.input("salary")
+    const dayWorks = request?.input("daysWorks")
+    const pension = request?.input("pension")
+    const salud = request?.input("salud")
+    const transport = request?.input("transport")
+    const comision = request?.input("comision")
+
+    console.log(transport, comision,)
+
+    if (name === null || salary === null) {
+      response
+        .status(400)
+        .json({ message: "El nombre no puede ser un valor vacio" });
+    }
 
     salarioModel.name =name;
     salarioModel.salary = salary;
-    salarioModel.dayWorks = dayWorks;
+    salarioModel.day_works = dayWorks;
     salarioModel.pension = pension;
     salarioModel.salud = salud;
 
-    transport === true ? salarioModel.transport = salary * 0.5 : 0;
-    comision === true ? salarioModel.comision = salary * 0.3 : 0;
-    await salarioModel.save()
+    if (transport) {
+      salarioModel.transport = salary * 0.1 
+    }else{
+      salarioModel.transport = 0
+    }
 
-    console.log(salarioModel)
-    response.status(200).json({ message: salarioModel.$isPersisted })
+    if (comision) {
+      salarioModel.comision = salary * 0.3 
+    }else{
+      salarioModel.comision = 0
+    }
+    
+    const newSalary = await salarioModel.save()
+    await newSalary.related('user').associate(user)
+
+
+    response.status(201).json({ message: "Salario creado con exito", newSalary })
   }
 
 
@@ -45,7 +68,6 @@ export default class ProyeccionSalariosController {
     const salarioDel = await Salario.findByOrFail("id", id);
     salarioDel.delete();
     response.status(200).json({ message: "Salario eliminado" });
-    console.log(salarioDel);
   }
 
 }
