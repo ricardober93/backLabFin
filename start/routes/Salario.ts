afa6f7ab573f13b144f022c74c002063baa25bfd
ricardober93 <ricardobzorro@gmail.com>
2021-08-05 11:30:13 +0000

    feat: se agrega la ruta de update salario

--- start/routes/Salario.ts
+++ start/routes/Salario.ts
@@ -5,6 +5,6 @@ const salarioController = new ProyeccionSalariosController();
 Route.group(() => {
   Route.get("/proyeccion/salarios", salarioController.index);
   Route.post("/proyeccion/salario", salarioController.create);
-
+  Route.put("/proyeccion/salario/:id", salarioController.update);
   Route.delete("/proyeccion/salario/:id", salarioController.destroy);
 }).middleware("auth:api");
