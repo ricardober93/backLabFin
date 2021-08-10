579145a78f8131e84d1bc09aa5f06696d8d6007d
ricardober93 <ricardobzorro@gmail.com>
2021-08-10 15:00:30 +0000

    feat: se crea el metodo de create en el variableController

--- app/Controllers/Http/ProyeccionVariablesController.ts
+++ app/Controllers/Http/ProyeccionVariablesController.ts
@@ -1,10 +1,26 @@
+import User  from 'App/Models/user';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Variable from 'App/Models/Variable';
 
 export default class ProyeccionVariablesController {
   public async index ({}: HttpContextContract) {
   }
 
-  public async create ({}: HttpContextContract) {
+  public async create ({request, response, auth }: HttpContextContract) {
+    const user: User = auth?.use("api")?.user;
+
+    const variable = request.all();
+
+    if (variable === null ) {
+      response
+        .status(400)
+        .json({ message: "No se puede crear una varibale vacia" });
+        return
+    }
+
+    const newVariable = await Variable.create(variable);
+    await newVariable.related('user').associate(user)
+
   }
 
   public async store ({}: HttpContextContract) {
