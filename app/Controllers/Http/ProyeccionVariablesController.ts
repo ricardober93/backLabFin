42016cbf5c70ec76d685e90b390c8221eab052bd
ricardober93 <ricardobzorro@gmail.com>
2021-08-10 16:28:00 +0000

    feat: se implementa el metodo index de variables

--- app/Controllers/Http/ProyeccionVariablesController.ts
+++ app/Controllers/Http/ProyeccionVariablesController.ts
@@ -4,7 +4,16 @@ import Variable from 'App/Models/Variable';
 
 const variableModel = new Variable();
 export default class ProyeccionVariablesController {
-  public async index ({}: HttpContextContract) {
+  public async index ({response, auth }: HttpContextContract) {
+    const user = await User.find(auth.use("api")?.user?.id);
+
+    await user?.load("variables");
+    if (user?.variables === null) {
+      response.status(400).json({ message: "no hay variables para mostrar" });
+    }
+    if (user?.variables !== null) {
+      response.status(200).json(user?.variables);
+    }
   }
 
   public async create ({request, response, auth }: HttpContextContract) {
